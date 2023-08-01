bd2c1b01fc143aa782152ed96c5ac089adad846a
Dee <deanna.iannantuono@gmail.com>
2023-08-01 20:08:01 +1000

    added a users app, updated projects and pledges

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from django.apps import apps
 
 class PledgeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.id')
     class Meta:
         model = apps.get_model('projects.Pledge')
         fields = '__all__'
 
-
 class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.id')
     class Meta:
         model = apps.get_model('projects.Project')
         fields ='__all__'
